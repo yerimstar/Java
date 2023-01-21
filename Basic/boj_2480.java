f1a7b363022212f4414ae4353f134c18850e1ef6
yerimstar <yerimhuh@naver.com>
2023-01-21 13:36:19 +0900

    [230121] 조건문

---
+++ Basic/boj_2480.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_2480 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        if (A == B) {
+            System.out.print(B == C ? 10000 + A * 1000 : 1000 + A * 100);
+        } else if (B == C || A == C) {
+            System.out.print(1000 + C * 100);
+        } else{
+            System.out.print(Math.max(Math.max(A,B),C)*100);
+        }
+    }
+
+}
