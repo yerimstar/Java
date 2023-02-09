044c7efd5be1d0133cde0c36a06a39fa14ff11b0
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-02-10 00:04:28 +0900

    [Gold V] Title: 신기한 소수, Time: 76 ms, Memory: 11396 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -35,7 +35,7 @@ public class Main {
         if(n <= 1){
             return false;
         }
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0){
                 return false;
             }
