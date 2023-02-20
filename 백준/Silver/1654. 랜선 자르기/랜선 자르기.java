ba615678d92a22ffe7fe7a117b9d4614ee08107f
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-02-20 19:47:37 +0900

    [Silver II] Title: 랜선 자르기, Time: 136 ms, Memory: 14840 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -4,43 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static int k, n;
+    static int k;
     static int[] lan;
-
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(bf.readLine(), " ");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         k = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        long lo = 0;
-        long hi = 0;
-        long mid;
+        long n = Integer.parseInt(st.nextToken());
+        long s = 0, e= 0, mid = 0;
         lan = new int[k];
-        for (int i = 0; i < k; i++) {
-            int l = Integer.parseInt(bf.readLine());
-            if (l > hi)
-                hi = l;
-            lan[i] = l;
+        for(int i = 0; i < k; i++) {
+            int num = Integer.parseInt(br.readLine());
+            lan[i] = num;
+            e = Math.max(num, e);
         }
-        hi++; // mid값 0 방지 
-        while (lo < hi) {
-            mid = (lo + hi) / 2;
-            if (upperBound(mid) < n) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
+        e++;
+        while(s < e) {
+            mid = (s+e)/2;
+            if(check(mid) < n) {
+                e = mid;
+            }else {
+                s = mid + 1;
             }
         }
-        sb.append(lo - 1);
-        System.out.println(sb.toString());
+        System.out.println(s-1);
     }
-
-    private static long upperBound(long mid) {
+    public static long check(long mid) {
         long cnt = 0;
-        for (int l : lan) {
-            cnt += (l / mid);
+        for(int i = 0; i < k; i++) {
+            cnt += (lan[i]/mid);
         }
         return cnt;
     }
