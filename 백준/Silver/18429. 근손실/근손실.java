65d5f81749c3e40f884f7cf453c428a650d889a9
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-02-21 14:27:58 +0900

    [Silver III] Title: 근손실, Time: 88 ms, Memory: 11852 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.java
+++ 백준/Silver/18429. 근손실/근손실.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int n,k, answer, weight;
-	static int[] kit, result;
+	static int[] kit;
 	static boolean[] checked;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +13,6 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		kit = new int[n];
-		result = new int[n];
 		checked = new boolean[n];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
@@ -31,7 +30,6 @@ public class Main {
 		for(int kitNo = 0; kitNo < n; kitNo++) {
 			if(promising(kitNo) && !checked[kitNo]) {
 				weight = weight + kit[kitNo];
-				result[day] = kitNo;
 				checked[kitNo] = true;
 				exercise(day+1);
 				weight = weight - kit[kitNo];
