2cae683f72ce5c1a2f45e9f8248b4a0a2a342821
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-02-27 22:27:52 +0900

    [Gold V] Title: 집합의 표현, Time: 396 ms, Memory: 53636 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        // Make-Set
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int check = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (check == 0) {
+                union(a, b);
+            } else if (check == 1) {
+                if (find(a) == find(b)) {
+                    sb.append("YES").append("\n");
+                } else {
+                    sb.append("NO").append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a > b) {
+            parent[a] = b;
+        } else {
+            parent[b] = a;
+        }
+    }
+
+    private static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        } else {
+            return parent[x] = find(parent[x]);
+        }
+    }
+}
