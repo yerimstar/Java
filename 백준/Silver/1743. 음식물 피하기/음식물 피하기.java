f6f3c61280e744a6e69ff3ffad4a53b5b0be1be3
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-04-12 23:25:28 +0900

    [Silver I] Title: 음식물 피하기, Time: 120 ms, Memory: 13164 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,6 +10,14 @@ public class Main {
     static int[][] map;
     static int[] mx = {1,-1,0,0};
     static int[] my = {0,0,1,-1};
+    static class Pos{
+        int x;
+        int y;
+        Pos(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,23 +37,28 @@ public class Main {
             for(int j = 0; j < m; j++){
                 cnt = 0;
                 if(map[i][j] ==1){
-                    dfs(i,j);
+                    bfs(i,j);
                 }
                 result = Math.max(result,cnt);
             }
         }
         System.out.println(result);
     }
-    private static void dfs(int x, int y){
-        cnt++;
-        map[x][y] = 0;
-        for(int i = 0;i < 4; i++){
-            int dx = x + mx[i];
-            int dy = y + my[i];
-            if(dx < 0 || dx >= n || dy < 0 || dy >= m)
-                continue;
-            if(map[dx][dy] == 1){
-                dfs(dx,dy);
+    private static void bfs(int x, int y){
+        Queue<Pos> q = new ArrayDeque<Pos>();
+        q.offer(new Pos(x,y));
+        while(!q.isEmpty()){
+            Pos cur = q.poll();
+            for(int i = 0;i < 4; i++){
+                int dx = cur.x + mx[i];
+                int dy = cur.y + my[i];
+                if(dx < 0 || dx >= n || dy < 0 || dy >= m)
+                    continue;
+                if (map[dx][dy] == 1) {
+                    cnt++;
+                    map[dx][dy] = 0;
+                    q.offer(new Pos(dx,dy));
+                }
             }
         }
     }
