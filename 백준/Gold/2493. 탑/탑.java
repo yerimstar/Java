8efed01610a01102e234116a4ab1bd1233900533
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-06-27 14:48:36 +0900

    [Gold V] Title: 탑, Time: 684 ms, Memory: 108120 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,50 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Top{
-    private int num;
-    private int index;
-
-    Top(int num, int index){
-        this.num = num;
-        this.index = index;
-    }
-
-    public int getNum() {
-        return num;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-}
 public class Main {
-    public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Top> stack = new Stack<>();
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++){
-            int num = Integer.parseInt(st.nextToken());
-            while(!stack.isEmpty()){
-                if(stack.peek().getNum() < num){
-                    stack.pop();
-                }else{
-                    break;
-                }
-            }
-            if(stack.isEmpty()){
-                sb.append("0").append(" ");
-            }else{
-                sb.append(stack.peek().getIndex()).append(" ");
-            }
-            stack.push(new Top(num,i));
+    static class Top{
+        private int height;
+        private int num;
 
+        public Top(int height,int num){
+            this.height = height;
+            this.num = num;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Top> stack = new Stack<Top>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i < n;i++){
+            int num = Integer.parseInt(st.nextToken());
+            if(stack.isEmpty()){
+               stack.push(new Top(num,i+1));
+               sb.append(0).append(" ");
+            }else{
+                while(!stack.isEmpty()){
+                    if(num > stack.peek().height){
+                        stack.pop();
+                    }else{
+                        sb.append(stack.peek().num).append(" ");
+                        break;
+                    }
+                }
+                if(stack.isEmpty()){
+                    sb.append(0).append(" ");
+                }
+                stack.push(new Top(num, i+1));
+            }
         }
         System.out.println(sb);
     }
