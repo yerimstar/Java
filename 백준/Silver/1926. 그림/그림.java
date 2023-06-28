d9b795afd5b03bd36a6f1006a4385f1b24f98dbb
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-06-28 17:51:27 +0900

    [Silver I] Title: 그림, Time: 368 ms, Memory: 44972 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -6,61 +6,68 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node{
-        int x,y;
-        public Node(int x,int y){
+    static int n,m;
+    static int[][] board;
+    static boolean[][] visited;
+    static int[][] mv = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    static class Pos{
+        private int x;
+        private int y;
+
+        public Pos(int x, int y){
             this.x = x;
             this.y = y;
         }
     }
-    static int n,m;
-    static int[][] map;
-    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        visited = new boolean[n][m];
+        board = new int[n][m];
         for(int i = 0;i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m;j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < m; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int ans1 = 0;
-        int ans2 = 0;
-        for(int i = 0;i < n;i++){
-            for(int j = 0; j < m;j++){
-                if(!visited[i][j] && map[i][j] == 1){
-                    visited[i][j] = true;
-                    ans1 = Math.max(ans1,bfs(i,j));
-                    ans2++;
-                }
+        visited = new boolean[n][m];
+        int ans = 0;
+        int cnt = 0;
+        for(int i = 0;i < n; i++){
+            for(int j = 0;j < m; j++){
+                // 방문했거나, 색칠 안된 부분 넘어가기
+                if(visited[i][j] || board[i][j] == 0)
+                    continue;
+                ans = Math.max(ans, bfs(i,j));
+                cnt++;
             }
         }
-        sb.append(ans2).append("\n").append(ans1);
-        System.out.println(sb);
+        System.out.println(cnt);
+        System.out.println(ans);
     }
     private static int bfs(int x, int y){
-        int[][] mv = {{0,1},{0,-1},{1,0},{-1,0}};
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(new Node(x,y));
-        int cnt = 1;
+        Queue<Pos> q = new ArrayDeque<>();
+        q.offer(new Pos(x,y));
+        visited[x][y] = true;
+        int cnt = 0;
         while(!q.isEmpty()){
-            Node cur = q.poll();
-            for(int i = 0; i < 4; i++){
-                int dx = cur.x + mv[i][0];
-                int dy = cur.y + mv[i][1];
+            Pos p = q.poll();
+            cnt++;
+            for(int i = 0;i < 4; i++){
+                int dx = p.x + mv[i][0];
+                int dy = p.y + mv[i][1];
+                // 그림 범위 벗어난 경우 넘어가기
                 if(dx < 0 || dx >= n || dy < 0 || dy >= m)
                     continue;
-                if(!visited[dx][dy] && map[dx][dy] == 1){
-                    visited[dx][dy] = true;
-                    q.offer(new Node(dx,dy));
-                    cnt++;
-                }
+                // 방문했거나, 색칠 안된 부분 넘어가기
+                if(visited[dx][dy] || board[dx][dy] == 0)
+                    continue;
+                // 방문처리
+                visited[dx][dy] = true;
+                // 큐에 삽입
+                q.offer(new Pos(dx,dy));
             }
         }
         return cnt;
