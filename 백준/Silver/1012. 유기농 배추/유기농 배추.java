f5646f7e2ce8cc4e760b318c0f0b8439ecc14a1a
허예림 <35604947+yerimstar@users.noreply.github.com>
2023-06-30 17:08:57 +0900

    [Silver II] Title: 유기농 배추, Time: 116 ms, Memory: 13516 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,58 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+// 1로 이어진 영역이 몇개인지 찾는 문제
 public class Main {
-    static int[][] move = {{0,1},{1,0},{0,-1},{-1,0}};
     static int n,m;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[][] mv = {{1,0},{0,1},{-1,0},{0,-1}};
+    static class Pos{
+        private int x;
+        private int y;
+        public Pos(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int tc = 0; tc < t; tc++){
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             m = Integer.parseInt(st.nextToken());
             n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
-            int x,y;
-            int[][] arr = new int[n][m];
-            boolean[][] visited = new boolean[n][m];
-            for(int i = 0; i < k; i++){
+            map = new int[n][m];
+            visited = new boolean[n][m];
+            for(int i = 0;i < k; i++){
                 st = new StringTokenizer(br.readLine());
-                x = Integer.parseInt(st.nextToken());
-                y = Integer.parseInt(st.nextToken());
-                arr[y][x] = 1;
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[y][x] = 1;
             }
             int cnt = 0;
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(arr[i][j] == 1){
-                        if(dfs(arr,visited,i,j)){
-                            cnt += 1;
-                        }
-                    }
+            for(int i = 0;i < n; i++){
+                for(int j = 0;j < m; j++){
+                    if(visited[i][j] || map[i][j] == 0)
+                        continue;
+                    bfs(i,j);
+                    cnt++;
                 }
             }
             sb.append(cnt).append("\n");
         }
         System.out.println(sb);
     }
-
-    public static boolean dfs(int[][] arr,boolean[][] visited, int y, int x) {
-        for(int i = 0; i < 4; i++){
-            int dy = y + move[i][0];
-            int dx = x + move[i][1];
-            if(0 <= dy && dy < n && 0 <= dx  && dx < m){
-                if(!visited[dy][dx] && arr[dy][dx] == 1) {
-                    arr[dy][dx] = 0;
-                    visited[dy][dx] = true;
-                    dfs(arr, visited, dy, dx);
-                }
+    private static void bfs(int x, int y){
+        Queue<Pos> q = new ArrayDeque<>();
+        q.offer(new Pos(x,y));
+        visited[x][y] = true;
+        while(!q.isEmpty()){
+            Pos p = q.poll();
+            for(int i = 0;i < 4; i++){
+                int dx = p.x + mv[i][0];
+                int dy = p.y + mv[i][1];
+                if(dx < 0 || dx >= n || dy < 0 || dy >= m)
+                    continue;
+                if(visited[dx][dy] || map[dx][dy] == 0)
+                    continue;
+                visited[dx][dy] = true;
+                q.offer(new Pos(dx,dy));
             }
         }
-        return true;
     }
 }
